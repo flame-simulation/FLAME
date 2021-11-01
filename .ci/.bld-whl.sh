a4f0b3e3154f5065209746e53c1c73a19a66c84d
Tong Zhang <zhangt@frib.msu.edu>
2021-11-01 16:30:39 -0400

    CI: Use manylinux2014 with libpython to build for py36-310.
    - drop support for py35.

--- .ci/.bld-whl.sh
+++ .ci/.bld-whl.sh
@@ -10,36 +10,16 @@ function repair_wheel {
     fi
 }
 
-function install {
-    major=$1
-    minor=$2
-    if [[ $major$minor == "35" || $major$minor == "36" ]]; then
-        yum install -y \
-            rh-python${PY_MAJOR}${PY_MINOR}-numpy
-    elif [[ $major$minor == "38" ]]; then
-        yum install -y \
-            rh-python${PY_MAJOR}${PY_MINOR}-python-numpy
-    fi
-}
+PYBIN=/opt/python/${PY_NAME}/bin
+PIP=${PYBIN}/pip
 
-yum install -y rh-python${PY_MAJOR}${PY_MINOR}-python-devel \
-               rh-python${PY_MAJOR}${PY_MINOR}-python-pip \
-               boost-devel flex bison doxygen
+yum install -y boost-devel flex bison doxygen cppcheck
+${PIP} install -r /io/requirements.txt
 
-install ${PY_MAJOR} ${PY_MINOR}
-
-. /opt/rh/rh-python${PY_MAJOR}${PY_MINOR}/enable
-
-pip${PY_MAJOR}.${PY_MINOR} install pip --upgrade
-pip${PY_MAJOR} install cmake wheel nose
-
-python_exe=`which python${PY_MAJOR}.${PY_MINOR}`
-cmake_exe=`which cmake`
-
-pip${PY_MAJOR} wheel /io/ --no-deps -w wheelhouse
+${PIP} wheel /io/ --no-deps -w wheelhouse
 
 unzip wheelhouse/*.whl -d flame0
-mv flame0/flame/*.so* /usr/lib
+mv flame0/flame/*.so* /usr/local/lib64
 
 for whl in wheelhouse/*$PY_MAJOR$PY_MINOR*.whl; do
     repair_wheel "$whl"
