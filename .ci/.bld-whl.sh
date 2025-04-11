3913c71797ad1f8158809828b3805b0375975077
Tong Zhang <zhangt@frib.msu.edu>
2025-04-11 15:49:34 -0400

    CI: Refine the wheel building script.

--- .ci/.bld-whl.sh
+++ .ci/.bld-whl.sh
@@ -1,45 +1,6 @@
 #!/bin/bash
 set -ex
 
-## Fix yum repo: CentOS 7
-## https://www.veeble.com/kb/centos-repo-list-working-urls/
-#rm -rf /etc/yum.repos.d/*
-#cat << 'EOF' > /etc/yum.repos.d/CentOS-Base.repo
-#[base]
-#name=CentOS-$releasever - Base
-#baseurl=https://vault.centos.org/7.9.2009/os/$basearch/
-#gpgcheck=1
-#gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
-#enabled=1
-#
-#[updates]
-#name=CentOS-$releasever - Updates
-#baseurl=https://vault.centos.org/7.9.2009/updates/$basearch/
-#gpgcheck=1
-#gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
-#enabled=1
-#
-#[extras]
-#name=CentOS-$releasever - Extras
-#baseurl=https://vault.centos.org/7.9.2009/extras/$basearch/
-#gpgcheck=1
-#gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
-#enabled=1
-#
-#[centosplus]
-#name=CentOS-$releasever - CentOSPlus
-#baseurl=https://vault.centos.org/7.9.2009/centosplus/$basearch/
-#gpgcheck=1
-#gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
-#enabled=0
-#EOF
-#
-#cat /etc/yum.repos.d/CentOS-Base.repo
-
-# yum clean all
-# yum makecache
-
-
 function repair_wheel {
     wheel="$1"
     if ! auditwheel show "$wheel"; then
@@ -52,10 +13,8 @@ function repair_wheel {
 PYBIN=/opt/python/${PY_NAME}/bin
 PIP=${PYBIN}/pip
 
-yum install -y boost-devel flex bison doxygen
-# skip cppcheck as it is not available now.
+yum install -y boost-devel flex bison doxygen cppcheck
 ${PIP} install -r /io/requirements/${PY_NAME}.txt
-
 ${PIP} wheel /io/ --no-deps -w wheelhouse
 
 unzip wheelhouse/*.whl -d flame0
