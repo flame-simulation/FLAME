ceb80adcbfe56626c1b369f0ecaa5e85110ebe98
Tong Zhang <zhangt@frib.msu.edu>
2026-08-31 15:54:22 -0400

    build(windows): support x64 CLI and CPython extension

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ import os
 import sys
 import shutil
 import pathlib
-import subprocess
+import sysconfig
+import shlex
 
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext as build_ext_orig
@@ -18,52 +19,61 @@ class build_ext(build_ext_orig):
     def run(self):
         for ext in self.extensions:
             self.build_cmake(ext)
-        super().run()
 
     def build_cmake(self, ext):
-        cwd = pathlib.Path().absolute()
-        build_temp = pathlib.Path(self.build_temp)
+        source_dir = pathlib.Path(__file__).resolve().parent
+        build_temp = pathlib.Path(self.build_temp).resolve()
         build_temp.mkdir(parents=True, exist_ok=True)
-        extdir = pathlib.Path(self.get_ext_fullpath(ext.name))
+        extpath = pathlib.Path(self.get_ext_fullpath(ext.name)).resolve()
+        extdir = extpath.parent
         extdir.mkdir(parents=True, exist_ok=True)
-        libpath = extdir.parent.joinpath("flame").absolute()
         config = 'Debug' if self.debug else 'Release'
-        pyexe = sys.executable
+        pyexe = pathlib.Path(sys.executable).resolve()
         cmake = shutil.which('cmake')
+        if not cmake:
+            raise RuntimeError('CMake 3.18 or newer is required')
+
         cmake_args = [
-            '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + str(libpath),
+            '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + str(extdir),
+            '-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=' + str(extdir),
+            '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_' + config.upper() + '=' + str(extdir),
+            '-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_' + config.upper() + '=' + str(extdir),
             '-DCMAKE_BUILD_TYPE=' + config,
-            '-DPYTHON_EXECUTABLE=' + pyexe,
+            '-DPython3_EXECUTABLE=' + str(pyexe),
+            '-DFLAME_PYTHON_EXTENSION_SUFFIX=' + (sysconfig.get_config_var('EXT_SUFFIX') or extpath.suffix),
+            '-DBUILD_TESTING=OFF',
+            '-DUSE_PYTHON=ON',
             '-DNEED_PYTHON=ON',
             '-DNEED_DEMOIOC=OFF',
+            '-DUSE_EPICS=OFF',
             '-DNEED_EPICS=OFF',
             '-DUSE_HDF5=OFF',
-            '-DDEF_PATH=/etc/flame/cavity_data',
         ]
+        cmake_args.extend(shlex.split(os.environ.get('CMAKE_ARGS', '')))
         build_args = [
-            "--config", config,
-            "--", '-j4'
+            '--config', config,
+            '--target', '_internal',
+            '--parallel', os.environ.get('CMAKE_BUILD_PARALLEL_LEVEL', '4'),
         ]
-        os.chdir(str(build_temp))
-        self.spawn([cmake, str(cwd)] + cmake_args)
-        env = os.environ.copy()
-        env['LD_LIBRARY_PATH'] = env.get('LD_LIBRARY_PATH', '') + ":" + str(libpath)
+        self.spawn([cmake, '-S', str(source_dir), '-B', str(build_temp)] + cmake_args)
         if not self.dry_run:
-            self.spawn([cmake, '--build', '.'] + build_args)
-            subprocess.run([shutil.which('ctest'), '--output-on-failure'], env=env)
-        os.chdir(str(cwd))
+            self.spawn([cmake, '--build', str(build_temp)] + build_args)
+
+        if not extpath.exists():
+            raise RuntimeError('CMake did not produce the expected extension: ' + str(extpath))
 
 
 setup(
     name='flame-code',
-    version='1.9.2',
+    version='1.9.3',
     package_dir={'flame': 'python/flame'},
     packages=['flame'],
-    ext_modules=[CMakeExtension('flame_core')],
+    ext_modules=[CMakeExtension('flame._internal')],
     cmdclass={
         'build_ext': build_ext,
     },
     install_requires = [
-        'numpy>1.21,<2.0',
-    ]
+        'numpy>=1.23.5,<2.0',
+    ],
+    python_requires='>=3.11',
 )
